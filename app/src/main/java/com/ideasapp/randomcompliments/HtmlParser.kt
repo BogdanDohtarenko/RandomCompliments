933e3878a295a269283fd0ba826713adeb00c13f
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2025-05-19 17:38:59 +0300

    HtmlParser

---
+++ app/src/main/java/com/ideasapp/randomcompliments/HtmlParser.kt
@@ -0,0 +1,22 @@
+package com.ideasapp.randomcompliments
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+
+object HtmlParser {
+    fun String.extractCompliments():List<String> {
+        val compliments:MutableList<String> = ArrayList()
+        val document:Document = Jsoup.parse(this)
+
+        // Select `<li>` elements inside `<ol>` with the class `wp-block-list`
+        val listItems:Elements = document.select("ol.wp-block-list > li")
+
+        for(i in listItems.indices) {
+            compliments.add(listItems[i].text())
+        }
+
+        return compliments
+    }
+}
