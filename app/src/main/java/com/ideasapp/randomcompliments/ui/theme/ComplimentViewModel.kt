188747b33e0684699dc03ceebeea723b66304afe
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2025-05-21 10:55:55 +0300

    coroutines added

--- app/src/main/java/com/ideasapp/randomcompliments/ui/theme/ComplimentViewModel.kt
+++ app/src/main/java/com/ideasapp/randomcompliments/ui/theme/ComplimentViewModel.kt
@@ -4,39 +4,53 @@ import android.util.Log
 import com.ideasapp.randomcompliments.ApiService
 import com.ideasapp.randomcompliments.HtmlParser.extractCompliments
 import com.ideasapp.randomcompliments.RetrofitClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
 class ComplimentViewModel {
+    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
+    var loadingCompleted: Boolean = false
     init {
-        val apiService:ApiService = RetrofitClient.retrofit.create(ApiService::class.java)
-        val call:Call<String> = apiService.getComplimentsPage()
+        coroutineScope.launch {
+            val apiService:ApiService = RetrofitClient.retrofit.create(ApiService::class.java)
+            val call:Call<String> = apiService.getComplimentsPage()
 
-        call.enqueue(object : Callback<String?> {
-            override fun onResponse(call:Call<String?>,response:Response<String?>) {
-                if (response.isSuccessful && response.body() != null) {
-                    val string = response.body()
-                    val compliments: List<String> = string?.extractCompliments() ?: listOf()
-                    var count = 0
-                    for(compliment in compliments) {
-                        count++
-                        Log.d(TAG,"$count: $compliment")
+            call.enqueue(object : Callback<String?> {
+                override fun onResponse(call:Call<String?>,response:Response<String?>) {
+                    if (response.isSuccessful && response.body() != null) {
+                        val string = response.body()
+                        val compliments: List<String> = string?.extractCompliments() ?: listOf()
+                        var count = 0
+                        for(compliment in compliments) {
+                            count++
+                            Log.d(TAG,"$count: $compliment")
+                        }
+                        loadingCompleted = true
+                    } else {
+                        Log.e(TAG,"Failed to fetch HTML content")
                     }
-                } else {
-                    Log.e(TAG,"Failed to fetch HTML content")
                 }
-            }
 
-            override fun onFailure(call:Call<String?>,t:Throwable) {
-                Log.e(TAG,"API call failed",t)
-            }
-        })
+                override fun onFailure(call:Call<String?>,t:Throwable) {
+                    Log.e(TAG,"API call failed",t)
+                }
+            })
+        }
     }
+
     fun getNewCompliment(): String {
-        return "I love you"
+        if (loadingCompleted)
+            return "I love you"
+        else
+            return "Please click again later"
     }
+
     companion object {
         const val TAG:String = "CallCompliment"
     }
--- app/src/main/java/com/ideasapp/randomcompliments/ui/theme/ui/MainScreen.kt
+++ app/src/main/java/com/ideasapp/randomcompliments/ui/theme/ui/MainScreen.kt
@@ -1,27 +1,41 @@
 package com.ideasapp.randomcompliments.ui.theme.ui
 
 import android.util.Log
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.ideasapp.randomcompliments.R
 import com.ideasapp.randomcompliments.ui.theme.ComplimentViewModel
 
 @Composable
 fun MainScreen() {
     val viewModel = ComplimentViewModel()
-    val currentCompliment = remember { mutableStateOf("!") }
+    val currentCompliment = remember { mutableStateOf("Click button please") }
     Log.d("ViewModel", "currentCompliment: $currentCompliment")
     Box(
         contentAlignment = Alignment.Center,
@@ -47,6 +61,31 @@ fun MainScreen() {
             ) {
                 Text(text = "Find new compliment")
             }
+            Spacer(modifier = Modifier.height(20.dp))
+            if (currentCompliment.value == "Click button please" || currentCompliment.value == "Please click again later")
+                BeatingHeartAnimation()
         }
     }
+}
+
+@Composable
+fun BeatingHeartAnimation() {
+    val infiniteTransition = rememberInfiniteTransition(label = "tractor")
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 0.8f,
+        targetValue = 1.2f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 500, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "tractor"
+    )
+
+    Image(
+        painter = painterResource(id = R.drawable.tractor),
+        contentDescription = "Beating Heart",
+        modifier = Modifier
+            .size(100.dp)
+            .scale(scale)
+    )
 }
